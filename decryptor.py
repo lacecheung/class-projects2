8e23ad7ea6714baa25ed1b2dd7a5c66f549ec089
Lace Cheung <lacheung@VISA.COM>
2016-02-08 21:40:02 -0800

    pseudo code

--- decryptor.py
+++ decryptor.py
@@ -1,3 +1,24 @@
 sequence = raw_input("Enter the sequence that you want to decrypt: ")
-key1 = int(raw_input("Enter key value 1: "))
-key2 = int(raw_input("Enter key value 2: "))
+#key1 = int(raw_input("Enter key value 1: "))
+#key2 = int(raw_input("Enter key value 2: "))
+
+#step 1: reverse order:
+
+
+
+
+
+#step 2: convert to int
+
+
+
+
+
+#step 3: divide by key values
+
+
+
+
+
+
+#step 4: convert back to letters (chr())
